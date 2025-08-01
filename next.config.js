68dca275a99366eafca37b8dc86bd6dcc0928ba0
dancharles-codarity <daniel.c@codarity.com>
2025-08-01 16:09:03 +0100

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: false // Using pages router for simplicity
+  }
+}
+
+module.exports = nextConfig
